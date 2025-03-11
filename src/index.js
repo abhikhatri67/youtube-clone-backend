0ba04a577240e1661d26e277b8e71c45bb5bef2b
Abhishek Khatri <abhikhatri67@gmail.com>
2025-03-11 22:48:34 +0530

    utils added for api errors and response.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
 // Connecting to db.
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at PORT: ", process.env.PORT);
+    });
+  })
+  .catch((error) => console.log("Mongo db connection failed !!!", error));
